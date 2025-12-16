a8a9a3c018c7f565fc1a8b300d69ff3bafa03d6a
Chalmoviansky <chalmoviansky@mail.muni.cz>
2025-12-16 11:15:36 +0100

    Initial commit No. 25

--- Surprise.m
+++ Surprise.m
@@ -1,7 +1,7 @@
 % Run this code :)
 x = -2:0.001:2;
 y = (sqrt(cos(x)).*cos(200*x)+sqrt(abs(x))-0.7).*(4-x.*x).^0.01;
-plot(x,y,'b','LineWidth',2.5)
+plot(x,y,'g','LineWidth',2.5)
 title('Skvelá práca!')
 
 
